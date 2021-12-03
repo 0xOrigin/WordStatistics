1c9e547568f51bfb614b79590c9884968b224602
Acro146 <60201913+Acro146@users.noreply.github.com>
2021-12-03 22:38:46 +0200

    done

--- WordStatistics/src/Utility/FReader.java
+++ WordStatistics/src/Utility/FReader.java
@@ -2,74 +2,74 @@ package Utility;
 
 import SharedResources.DirectoryContainer;
 import SharedResources.FileContainer;
-
+import static Utility.Path.getdirType;
 import java.util.*;
 import java.io.*;
 
 public abstract class FReader implements Runnable {
 
-    private static String dirName, fileName, path;
+    private static String path ;
+    private static int dirType = 0 ;
 
     public static void filereader(String path, String dirName, String fileName) {
         FReader.path = path;
-        FReader.dirName = dirName;
-        FReader.fileName = fileName;
-        UpdateStatistics(path, dirName, fileName);
+        FReader.dirType = getdirType(path);
+        UpdateStatistics(dirType, dirName, fileName);
     }
 
-    private synchronized static void UpdateStatistics(String path, String dirName, String fileName) {
+    private synchronized static void UpdateStatistics(int dirType, String dirName, String fileName) {
         try {
             ArrayList<String> FileContent = ReadFromFile();
             String LONGEST_WORD = "", SHORTEST_WORD = "";
             for (String line : FileContent) {
                 String[] words = line.split(" ");
                 for (String word : words) {
-
-                    FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
-                    Utility.Column.NUMBER_OF_WORDS.ordinal());
                     
-                    DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
-                                    Utility.Column.NUMBER_OF_WORDS.ordinal());
+                    FileContainer.incrementCounter(dirType, dirName, fileName,
+                   Column.NUMBER_OF_WORDS.ordinal());
+                    
+                    DirectoryContainer.incrementCounter(dirType, dirName,
+                                   Column.NUMBER_OF_WORDS.ordinal());
 
                     switch (word.toLowerCase()) {
                         case "is": {
-                            FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
-                                    Utility.Column.NUMBER_OF_IS.ordinal());
+                            FileContainer.incrementCounter(dirType, dirName, fileName,
+                                   Column.NUMBER_OF_IS.ordinal());
 
-                            DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
-                                    Utility.Column.NUMBER_OF_IS.ordinal());
+                            DirectoryContainer.incrementCounter(dirType, dirName,
+                                   Column.NUMBER_OF_IS.ordinal());
 
                             break;
                         }
                         case "are": {
-                            FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
-                                    Utility.Column.NUMBER_OF_ARE.ordinal());
+                            FileContainer.incrementCounter(dirType, dirName, fileName,
+                                   Column.NUMBER_OF_ARE.ordinal());
 
-                            DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
-                                    Utility.Column.NUMBER_OF_ARE.ordinal());
+                            DirectoryContainer.incrementCounter(dirType, dirName,
+                                   Column.NUMBER_OF_ARE.ordinal());
 
                             break;
                         }
                         case "you": {
-                            FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
-                                    Utility.Column.NUMBER_OF_YOU.ordinal());
+                            FileContainer.incrementCounter(dirType, dirName, fileName,
+                                   Column.NUMBER_OF_YOU.ordinal());
 
-                            DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
-                                    Utility.Column.NUMBER_OF_YOU.ordinal());
+                            DirectoryContainer.incrementCounter(dirType, dirName,
+                                   Column.NUMBER_OF_YOU.ordinal());
 
                             break;
                         }
                         default: {
                             if (word.length() > LONGEST_WORD.length()) {
                                 LONGEST_WORD = word;
-                                FileContainer.storeLongestWord(Path.getdirType(path), dirName, fileName, LONGEST_WORD);
-                                DirectoryContainer.storeLongestWord(Path.getdirType(path), dirName, LONGEST_WORD);
+                                FileContainer.storeLongestWord(dirType, dirName, fileName, LONGEST_WORD);
+                                DirectoryContainer.storeLongestWord(dirType, dirName, LONGEST_WORD);
                             }
-                            if (word.length() < SHORTEST_WORD.length()) {
+                            if (word.length() < SHORTEST_WORD.length() || SHORTEST_WORD.length() == 0) {
                                 SHORTEST_WORD = word;
-                                FileContainer.storeShortestWord(Path.getdirType(path), dirName, fileName,
+                                FileContainer.storeShortestWord(dirType, dirName, fileName,
                                         SHORTEST_WORD);
-                                DirectoryContainer.storeLongestWord(Path.getdirType(path), dirName, SHORTEST_WORD);
+                                DirectoryContainer.storeLongestWord(dirType, dirName, SHORTEST_WORD);
                             }
                         }
                     }

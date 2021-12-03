f5519364cd4713896303467f3a0a955f7606474d
Acro146 <60201913+Acro146@users.noreply.github.com>
2021-12-03 21:50:22 +0200

    done

--- WordStatistics/src/Utility/FReader.java
+++ WordStatistics/src/Utility/FReader.java
@@ -1,16 +1,105 @@
 package Utility;
 
-public class FReader implements Runnable {
-    
-    private String path;
+import SharedResources.DirectoryContainer;
+import SharedResources.FileContainer;
 
-    public FReader(String path) {
-       this.path = path;
+import java.util.*;
+import java.io.*;
+
+public abstract class FReader implements Runnable {
+
+    private static String dirName, fileName, path;
+
+    public static void filereader(String path, String dirName, String fileName) {
+        FReader.path = path;
+        FReader.dirName = dirName;
+        FReader.fileName = fileName;
+        UpdateStatistics(path, dirName, fileName);
     }
-    
-    @Override
-    public void run(){
-        
+
+    private static void UpdateStatistics(String path, String dirName, String fileName) {
+        try {
+            ArrayList<String> FileContent = ReadFromFile();
+            String LONGEST_WORD = "", SHORTEST_WORD = "";
+            for (String line : FileContent) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+
+                    FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
+                    Utility.Column.NUMBER_OF_WORDS.ordinal());
+                    
+                    DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
+                                    Utility.Column.NUMBER_OF_WORDS.ordinal());
+
+                    switch (word.toLowerCase()) {
+                        case "is": {
+                            FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
+                                    Utility.Column.NUMBER_OF_IS.ordinal());
+
+                            DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
+                                    Utility.Column.NUMBER_OF_IS.ordinal());
+
+                            break;
+                        }
+                        case "are": {
+                            FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
+                                    Utility.Column.NUMBER_OF_ARE.ordinal());
+
+                            DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
+                                    Utility.Column.NUMBER_OF_ARE.ordinal());
+
+                            break;
+                        }
+                        case "you": {
+                            FileContainer.incrementCounter(Path.getdirType(path), dirName, fileName,
+                                    Utility.Column.NUMBER_OF_YOU.ordinal());
+
+                            DirectoryContainer.incrementCounter(Path.getdirType(path), dirName,
+                                    Utility.Column.NUMBER_OF_YOU.ordinal());
+
+                            break;
+                        }
+                        default: {
+                            if (word.length() > LONGEST_WORD.length()) {
+                                LONGEST_WORD = word;
+                                FileContainer.storeLongestWord(Path.getdirType(path), dirName, fileName, LONGEST_WORD);
+                                DirectoryContainer.storeLongestWord(Path.getdirType(path), dirName, LONGEST_WORD);
+                            }
+                            if (word.length() < SHORTEST_WORD.length()) {
+                                SHORTEST_WORD = word;
+                                FileContainer.storeShortestWord(Path.getdirType(path), dirName, fileName,
+                                        SHORTEST_WORD);
+                                DirectoryContainer.storeLongestWord(Path.getdirType(path), dirName, SHORTEST_WORD);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-    
+
+  
+    private static ArrayList<String> ReadFromFile() throws IOException {
+        ArrayList<String> FileContent = null;
+        File file = new File(path);
+        FileReader reader = null;
+        try {
+            reader = new FileReader(file);
+            char[] chars = new char[(int) file.length()];
+            reader.read(chars);
+            FileContent = new ArrayList<String>(Arrays.asList(new String(chars).split("\n")));
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        return FileContent;
+    }
+
 }

5f866a460d6a5cf76bd2f22bf7e573818b92ec78
xorigin <xorigin@0xOrigin>
2021-12-03 15:18:39 +0200

    Preparation to start work

--- WordStatistics/src/Utility/FReader.java
+++ WordStatistics/src/Utility/FReader.java
@@ -1,10 +1,10 @@
 package Utility;
 
-public class FileReader implements Runnable {
+public class FReader implements Runnable {
     
     private String dirName, fileName;
 
-    public FileReader(String dirName, String fileName) {
+    public FReader(String dirName, String fileName) {
         this.dirName = dirName;
         this.fileName = fileName;
     }
